9e8aa8bc748852885c10bd6042f0395dd00eccee
Cory Saunders <cnotefs43@fullsail.edu>
2015-03-23 20:16:15 -0400

    Fixing all the errors that previous commits were causing.

--- 1503-StructByLightning-LostSamurai-StrawManProject/Lost Samurai/Last Samurai/AnimationTimestamp.h
+++ 1503-StructByLightning-LostSamurai-StrawManProject/Lost Samurai/Last Samurai/AnimationTimestamp.h
@@ -1,8 +1,27 @@
 #pragma once
+#include <string>
+
 class AnimationTimestamp
 {
+private:
+	
+	std::string m_CurrentAnimation;
+	int			m_CurrentFrame;
+	float		m_TimeElapsedOnFrame;
 public:
-	AnimationTimestamp();
-	~AnimationTimestamp();
+	//Ctor and Dtor
+	AnimationTimestamp() = default;
+	~AnimationTimestamp() = default;
+
+	//Accessors
+	std::string		GetCurrAnim() const			{ return m_CurrentAnimation; }
+	int				GetCurrFrame() const		{ return m_CurrentFrame; }
+	float			GetTimeOnFrame() const		{ return m_TimeElapsedOnFrame; }
+
+	//Mutators
+	void			SetCurrAnim(std::string _string)		{ m_CurrentAnimation = _string; }
+	void			SetCurrFrame(int _FrameID)				{ m_CurrentFrame = _FrameID; }
+	void			SetElapsedTime(float _Time)				{ m_TimeElapsedOnFrame = _Time; }
+
 };
 
